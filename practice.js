a309a87c0e5a7d4754fbbf3aedac8fe29f510e5c
awdesh <awdesh.sharma1@gmail.com>
2016-09-04 13:20:34 -0700

    initial commit for online sshopping MEAN app

---
+++ practice.js
@@ -0,0 +1,62 @@
+var _ = require("underscore");
+var mongodb = require('mongodb');
+var uri = 'mongodb://localhost:27017/movies';
+mongodb.MongoClient.connect(uri, function(error, db) {
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+
+    var movie = {
+        'Name': 'Sholay',
+        'Year': '1992',
+        'Rating': 'PG',
+        'ratings': {
+            'critics': 70,
+            'audience': 95
+        },
+        'screenplay': ['Ramesh Sippy', 'Rohan Sippy']
+    };
+
+    db.collection('movies').insert(movie, function(err, result) {
+        if (err) {
+            console.log('error inserting movie doc.');
+            process.exit(1);
+        }
+
+        var query = {
+            'screenplay': 'Ramesh Sippy'
+        };
+        db.collection('movies').find(query).toArray(function(error, movies) {
+            if (error) {
+                console.log(errror);
+                process.exit(1);
+            }
+
+            console.log('Found movies-:');
+            _.each(movies, function(movie) {
+                console.log(JSON.stringify(movie));
+            });
+
+            query = {
+                'ratings.audience': {
+                    '$gte': 70
+                }
+            };
+
+            db.collection('movies').find(query).toArray(function(error, movies) {
+                if (error) {
+                    console.log(errror);
+                    process.exit(1);
+                }
+
+                console.log('Found movies by ratings-:');
+                _.each(movies, function(movie) {
+                    console.log(JSON.stringify(movie));
+                });
+
+                process.exit(0);
+            });
+        });
+    });
+});
